578e70903de5f644d4daf8d248b56d70b552ea5b
Shelmaaulia <114642004+Shelmaaulia@users.noreply.github.com>
2022-10-28 15:49:41 +0700

    Add files via upload

---
+++ TUGAS CPP 4_SHELMA AULIA (3).cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int kode;
+
+    cout << "Menu Restaurant MC'Yahud :" << endl;
+    cout << endl;
+    cout << "1: Nasi goreng informatika  Rp. 5000" << endl;
+    cout << "2: Nasi soto ayam internet  Rp. 7000" << endl;
+    cout << "3: Gado - gado disket       Rp. 4500" << endl;
+    cout << "4: Bubur ayam LAN           Rp. 4000" << endl;
+    cout << endl;
+    cout << "Masukan menu pilihan anda!" << endl;
+    cin >> kode;
+
+    if (kode == 1)
+        cout << "Pilihan No. 1 Nasi goreng informatika Rp. 5000" << endl;
+    else if (kode == 2)
+        cout << "Pilihan No. 2 Nasisot0 ayam internet Rp. 7000" << endl;
+    else if (kode == 3)
+        cout << "Pilihan No. 3 Gado - gado disket Rp. 4500" << endl;
+    else if (kode == 4)
+        cout << "Pilihan No. 4 Bubur ayam LAN Rp. 4000" << endl;
+
+    return 0;
+
+}
